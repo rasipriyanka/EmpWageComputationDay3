561860c4d0455bf5a226f03e3b649fc5fa4eecdb
priyanka <priyankapendela111@gmail.com>
2022-01-21 18:16:31 +0530

    [Rasi]Added Employee wage use case4

--- EmpWage/EmployeeWage.cs
+++ EmpWage/EmployeeWage.cs
@@ -14,36 +14,37 @@ namespace EmpWage
         /// ---------Instance Variables-------///
         /// </summary>
         const int Is_Full_Time = 1;
-        const int Emp_Rate_Per_Hour = 10;
         const int Is_Part_Time = 2;
-        //----------Method starts------------//
-        public void Compare()
+        const int Emp_Rate_Per_Hour = 10;
+        //---------Method Starts--------------//
+        public void EmpWage()
         {
             int empHrs = 0;
             int empWage = 0;
             Random randomNumber = new Random();
-            int checkingNum = randomNumber.Next(0, 2);
-            if (checkingNum == Is_Full_Time)
+            int checkingNum = randomNumber.Next(0, 3);
+            switch (checkingNum)
             {
-                Console.WriteLine("Emp present FullTime");
-                empHrs = 8;
-            }
-            else if (checkingNum ==Is_Part_Time)
-            {
-                empHrs = 4;
-                Console.WriteLine("Emp present partTime");
-            }
-            else
-            {
-                Console.WriteLine("Emp is absent");
-                empHrs = 0;
+                case Is_Full_Time:
+                    empHrs = 8;
+                    Console.WriteLine("Emp present FullTime");
+                    break;
+                case Is_Part_Time:
+                    empHrs = 4;
+                    Console.WriteLine("Emp present partTime");
 
+                    break;
+                default:
+                    empHrs = 0;
+                    Console.WriteLine("Emp absent");
+
+                    break;
             }
             empWage = empHrs * Emp_Rate_Per_Hour;
-            Console.WriteLine("Emp Wage: " + empWage);
+            Console.WriteLine("Emp Wage: \n" + empWage);
         }
     }
-    //--------------Method Ends-----------------//
+    //------------Method Ends------------------//
 }
 
 
